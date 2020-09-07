e975b9b1aeb7528ab08b41b59c60b64a66f42436
MasterJD <35749150+MasterJD@users.noreply.github.com>
2020-09-07 13:46:28 -0600

    Carrito Card V1

--- Front-end/Front-end/src/app/components/checkout/checkout.component.ts
+++ Front-end/Front-end/src/app/components/checkout/checkout.component.ts
@@ -11,30 +11,14 @@ export class CheckoutComponent implements OnInit {
   user = 'PlaceHolder';
   cards = [
     {
-      title: 'Card Title 1',
-      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
-      buttonText: 'Button',
-      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
-    },
-    {
-      title: 'Card Title 2',
-      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
-      buttonText: 'Button',
-      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
-    },
-    {
-      title: 'Card Title 3',
-      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
-      buttonText: 'Button',
-      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
-    },
-    {
-      title: 'Card Title 4',
-      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
-      buttonText: 'Button',
-      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
-    },
-
+      img: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      title: 'Montañas',
+      description: 'Paisaje de montañas',
+      category: 'Categoría: Fotografía',
+      author: 'Jhon Smith',
+      available: 'Cantidad Disponible: 10',
+      price: 'Precio: Q 100.00'
+    }
   ];
   constructor(private router: Router) { }
 
