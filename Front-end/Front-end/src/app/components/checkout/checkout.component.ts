0f45635ece7b8fd9188bdd2e56bd4867d79dfc64
MasterJD <35749150+MasterJD@users.noreply.github.com>
2020-09-06 19:21:56 -0600

    Update checkout.component.ts

--- Front-end/Front-end/src/app/components/checkout/checkout.component.ts
+++ Front-end/Front-end/src/app/components/checkout/checkout.component.ts
@@ -9,7 +9,33 @@ import { Router } from '@angular/router';
 export class CheckoutComponent implements OnInit {
 
   user = 'PlaceHolder';
+  cards = [
+    {
+      title: 'Card Title 1',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      buttonText: 'Button',
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+    },
+    {
+      title: 'Card Title 2',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      buttonText: 'Button',
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+    },
+    {
+      title: 'Card Title 3',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      buttonText: 'Button',
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+    },
+    {
+      title: 'Card Title 4',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      buttonText: 'Button',
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+    },
 
+  ];
   constructor(private router: Router) { }
 
   ngOnInit(): void {
