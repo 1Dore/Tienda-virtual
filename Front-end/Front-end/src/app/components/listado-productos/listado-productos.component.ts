5573577109446ad387fb9f217c48eafeac857833
JoseAbril321 <46824246+JoseAbril321@users.noreply.github.com>
2020-09-13 00:05:10 -0600

    Enviado de tipos a lista de productos

--- Front-end/Front-end/src/app/components/listado-productos/listado-productos.component.ts
+++ Front-end/Front-end/src/app/components/listado-productos/listado-productos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Route } from '@angular/compiler/src/core';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/service/auth/auth.service';
 
 interface producto{
   tipo: String;
@@ -19,13 +20,18 @@ interface producto{
 export class ListadoProductosComponent implements OnInit {
 
   lista_de_productos: Array<producto>;
-
+  tipo: String;
   user = 'PlaceHolder';
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private servicio:AuthService) { }
 
   ngOnInit(): void {
+    this.servicio.enviarTipo.subscribe(tipo => {
+      this.tipo = tipo;
+    })
   }
+
+
   irA(ruta: string) {
     this.router.navigateByUrl(ruta);
   }
