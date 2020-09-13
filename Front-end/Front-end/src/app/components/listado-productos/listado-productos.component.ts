4f01f776628f5d0da49d90dc09fb86b5a2ad2b5a
MasterJD <35749150+MasterJD@users.noreply.github.com>
2020-09-13 17:43:00 -0600

    Update listado-productos.component.ts

--- Front-end/Front-end/src/app/components/listado-productos/listado-productos.component.ts
+++ Front-end/Front-end/src/app/components/listado-productos/listado-productos.component.ts
@@ -18,14 +18,10 @@ interface producto {
   styleUrls: ['./listado-productos.component.scss']
 })
 export class ListadoProductosComponent implements OnInit {
-  //INPUT OUTPUT PRODUCTO
-  @Input() product: any;
-  @Output() productAdded = new EventEmitter();
 
   lista_de_productos: Array<producto>;
   tipo: String;
   user = 'PlaceHolder';
-  cartProductList = [];
 
   constructor(private router: Router, private servicio: AuthService) { }
 
@@ -39,20 +35,6 @@ export class ListadoProductosComponent implements OnInit {
 
   irA(ruta: string) {
     this.router.navigateByUrl(ruta);
-  }
-  //METODO QUE AÑADE PRODUCTOS DE LA LISTA DE PRODUCTOS AL CARRITO
-  addProductToCart(product) {
-    const productExistInCart = this.cartProductList.find(({ name }) => name === product.name); // find product by name
-    if (!productExistInCart) {
-      this.cartProductList.push({ ...product, num: 1 }); // enhance "porduct" opject with "num" property
-      this.productAdded.emit(product);
-      return;
-    }
-    productExistInCart.num += 1;
-  }
-  //METODO QUE REMUEVE LOS PRODUCTOS DEL CARRITO
-  removeProduct(product) {
-    this.cartProductList = this.cartProductList.filter(({ name }) => name !== product.name)
   }
 
 }
