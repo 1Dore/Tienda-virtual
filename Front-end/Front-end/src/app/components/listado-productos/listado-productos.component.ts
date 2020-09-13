dfb6f4baf79bf0cf387e2fd3e0b2c79ca5ef2b48
MasterJD <35749150+MasterJD@users.noreply.github.com>
2020-09-13 13:52:18 -0600

    Update listado-productos.component.ts

--- Front-end/Front-end/src/app/components/listado-productos/listado-productos.component.ts
+++ Front-end/Front-end/src/app/components/listado-productos/listado-productos.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Route } from '@angular/compiler/src/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/service/auth/auth.service';
 
-interface producto{
+interface producto {
   tipo: String;
   nombre: String;
   descripcion: String;
@@ -18,12 +18,16 @@ interface producto{
   styleUrls: ['./listado-productos.component.scss']
 })
 export class ListadoProductosComponent implements OnInit {
+  //INPUT OUTPUT PRODUCTO
+  @Input() product: any;
+  @Output() productAdded = new EventEmitter();
 
   lista_de_productos: Array<producto>;
   tipo: String;
   user = 'PlaceHolder';
+  cartProductList = [];
 
-  constructor(private router: Router, private servicio:AuthService) { }
+  constructor(private router: Router, private servicio: AuthService) { }
 
   ngOnInit(): void {
     this.servicio.enviarTipo.subscribe(tipo => {
@@ -36,4 +40,19 @@ export class ListadoProductosComponent implements OnInit {
   irA(ruta: string) {
     this.router.navigateByUrl(ruta);
   }
+  //METODO QUE AÑADE PRODUCTOS DE LA LISTA DE PRODUCTOS AL CARRITO
+  addProductToCart(product) {
+    const productExistInCart = this.cartProductList.find(({ name }) => name === product.name); // find product by name
+    if (!productExistInCart) {
+      this.cartProductList.push({ ...product, num: 1 }); // enhance "porduct" opject with "num" property
+      this.productAdded.emit(product);
+      return;
+    }
+    productExistInCart.num += 1;
+  }
+  //METODO QUE REMUEVE LOS PRODUCTOS DEL CARRITO
+  removeProduct(product) {
+    this.cartProductList = this.cartProductList.filter(({ name }) => name !== product.name)
+  }
+
 }
