66e853c494b565ab3369d2ae213244bd87aec282
1Dore <oswaldo.aquino@galileo.edu>
2020-09-15 23:59:38 -0600

    efsf

--- Front-end/Front-end/src/app/components/pago-producto/pago-producto.component.ts
+++ Front-end/Front-end/src/app/components/pago-producto/pago-producto.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { AuthService } from 'src/app/service/auth/auth.service';
+import { AuthService } from '../../service/auth/auth.service';
 import { formulario } from './formularioTarjeta';
 
 class Emisor {
@@ -134,3 +134,4 @@ export class PagoProductoComponent implements OnInit {
   }
 
 }
+
